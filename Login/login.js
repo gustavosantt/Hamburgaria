450869dc6bb7a027baa0381e3720b41de40ebf13
Gustavo <gustavo22532@gmail.com>
2025-06-16 15:34:34 -0300

    Atualizacao no footer e interação com js na tela de Logon

---
+++ Login/login.js
@@ -0,0 +1,18 @@
+function verificar() {
+    console.log("Função verificar foi chamada."); // Depuração
+
+    const login = document.getElementById("login").value;
+    const senha = document.getElementById("senha").value;
+
+    console.log("Login:", login, "Senha:", senha); // Depuração
+
+    if (login === "admin" && senha === "1234") {
+        alert("Login realizado com sucesso!");
+        console.log("Autorizado: Login realizado com sucesso."); // Depuração
+        window.location.href = "index.html"; 
+        return true; // Permite o envio do formulário
+        console.log("Não autorizado: Login ou senha incorretos."); 
+        alert("Login ou senha incorretos.");
+        return false; // Impede o envio do formulário
+    }
+}
